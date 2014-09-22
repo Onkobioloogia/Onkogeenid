90779ccea2774e01eee6b8e9ab668f9dbf6a7d59
Taavi Päll <tapa741@gmail.com>
2014-09-22 17:28:51 +0300

    publishing deck

---
+++ slidify-gitstuff.R
@@ -0,0 +1,12 @@
+library(slidify)
+setwd("~/Dropbox/Onkobioloogia/Onkogeenid/")
+publish(repo="Onkogeenid",user="tpall")
+
+## susapusa
+# system("ssh-add ~/.ssh/id_rsa")
+# system("ssh -T git@github.com")
+# system("git add .")
+# system("git commit -a -m \"publishing deck\"")
+# system("git push git@github.com:tpall/Onkogeenid gh-pages")
+# system("git fetch --all")
+# system("git pull origin gh-pages")
