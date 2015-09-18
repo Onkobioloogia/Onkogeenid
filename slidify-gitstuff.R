53258a39ee803e85473a5a132274600867526cad
Taavi Päll <tapa741@gmail.com>
2015-09-18 13:43:05 +0300

    comment off publish...

--- slidify-gitstuff.R
+++ slidify-gitstuff.R
@@ -1,6 +1,7 @@
 library(slidify)
+library(slidifyLibraries)
 setwd("~/Dropbox/Onkobioloogia/Onkogeenid/")
 slidify("index.Rmd")
 browseURL("index.html")
-publish(repo="Onkogeenid", user="tpall")
+# publish(repo="Onkogeenid", user="tpall")
 
