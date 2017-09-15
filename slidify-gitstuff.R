f19937d46d30843c8be7a536f31ead228487464e
Taavi Päll <tapa741@gmail.com>
2017-09-15 16:54:52 +0300

    delete

--- slidify-gitstuff.R
+++
@@ -1,7 +0,0 @@
-library(slidify)
-library(slidifyLibraries)
-setwd("~/Dropbox/Onkobioloogia/Onkogeenid/")
-slidify("index.Rmd")
-browseURL("index.html")
-# publish(repo="Onkogeenid", user="tpall")
-
